b38bc014d4e737a49d2f3f9780e9aaca5981fd63
Ray <rguo1010@uw.edu>
2025-09-17 20:04:45 -0500

    revamped darkmode toggle

--- my-portfolio/src/components/DarkModeToggle.jsx
+++ my-portfolio/src/components/DarkModeToggle.jsx
@@ -1,19 +1,19 @@
 import "../styles/components/DarkModeToggle.css";
-import sun from "../assets/sun.png";
-import moon from "../assets/moon.png";
+import "@theme-toggles/react/css/Within.css";
+import { Within } from "@theme-toggles/react";
 
 function DarkModeToggle({ darkMode, setDarkMode }) {
   return (
-    <button
-      className={`dark-mode-button${darkMode ? " dark" : ""}`}
+    <div
+      className="theme-toggle"
       onClick={() => setDarkMode((prev) => !prev)}
     >
-      {darkMode ? (
-        <img src={moon} alt="Dark mode" className="dark-mode-icon" />
-      ) : (
-        <img src={sun} alt="Light mode" className="dark-mode-icon" />
-      )}
-    </button>
+      <Within
+        duration={750}
+        toggled={darkMode}
+        className={`within-toggle ${darkMode ? "dark" : "light"}`}
+      />
+    </div>
   );
 }
 
